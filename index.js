abe29019dc9a248e7f739fd0806238f377fb00a5
Maestroharyor <haryor1josh@gmail.com>
2022-02-10 13:43:44 +0100

    Seeding 1

--- index.js
+++ index.js
@@ -5,40 +5,17 @@ const cors = require('cors');
 // const axios = require('axios');
 const dotenv = require('dotenv/config');
 const routes = require('./routes/api');
-// const {braandlySeeder} = require('./middlewares/seeding');
-// const User = require('./models/userModel');
-// const {user_seed_data} = require('./data/user_data');
-// const {blog_seed_data} = require('./data/blog_data');
-// const Audition = require('./models/auditionModel');
-// const {questionFetchQuestion} = require('./data/audition_data');
-// const Blog = require('./models/blogModel');
-// const {blogFetch} = require('./data/blog_data');
-
-
-// let audition_seed_data;
-
-// (async function() {
-//   try {
-//     audition_seed_data = await questionFetchQuestion();
-//     braandlySeeder(audition_seed_data, Audition)
-//     console.log({audition_seed_data})
-//   } catch (e) {
-//     return console.log(e);
-//   }
-// })()
-// console.log({audition_seed_data})
+const {braandlySeeder} = require('./middlewares/seeding');
+const User = require('./models/userModel');
+const {user_seed_data} = require('./data/user_data');
+const {blog_seed_data} = require('./data/blog_data');
+const Audition = require('./models/auditionModel');
+const {questionFetchQuestion} = require('./data/audition_data');
+const Blog = require('./models/blogModel');
+const {blogFetch} = require('./data/blog_data');
 
 
 
-// (async function() {
-//   try {
-//     let blog_seed_data = await blogFetch();
-//     braandlySeeder(blog_seed_data, Blog)
-//     // console.log({blog_seed_data})
-//   } catch (e) {
-//     return console.log(e);
-//   }
-// })()
 
 
 
@@ -72,7 +49,32 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   
     // console.log({audition_seed_data})
     // braandlySeeder(user_seed_data, Audition)
-    // braandlySeeder(audition_seed_data, User)
+    braandlySeeder(user_seed_data, User)
+
+    let audition_seed_data;
+
+(async function() {
+  try {
+    audition_seed_data = await questionFetchQuestion();
+    braandlySeeder(audition_seed_data, Audition)
+    console.log({audition_seed_data})
+  } catch (e) {
+    return console.log(e);
+  }
+})()
+console.log({audition_seed_data})
+
+
+
+(async function() {
+  try {
+    let blog_seed_data = await blogFetch();
+    braandlySeeder(blog_seed_data, Blog)
+    // console.log({blog_seed_data})
+  } catch (e) {
+    return console.log(e);
+  }
+})()
   })
   .catch((err) => console.log(err));
 
