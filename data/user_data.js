7e87334bb07ce004e8bb0eefcfd51b0cd8202689
Maestroharyor <haryor1josh@gmail.com>
2022-02-10 14:31:33 +0100

    Seeding 3

--- data/user_data.js
+++ data/user_data.js
@@ -1,31 +1,34 @@
+const faker = require("faker");
 
-const faker = require('faker');
+const createSeedData = (num = 500) => {
+  const user_role_types = ["subscriber", "contestant", "admin", "superadmin"];
+  const batch_types = ["A", "B", "C", "D"];
 
-const user_role_types = ['subscriber', 'contestant', 'admin', 'superadmin']
-const batch_types = ['A', 'B', 'C', 'D']
+  const user_seed_data = [];
 
-const user_seed_data = []
-
-for (let i=0; i<=500; i++){
-  const testData = {
-    firstname:faker.name.firstName(),
-    lastname:faker.name.lastName(),
-    username: `${faker.name.firstName()}-${faker.name.lastName()}`,
-    password: "123456",
-    email: faker.internet.email(),
-    user_role: user_role_types[Math.ceil(Math.random() * user_role_types.length) - 1],
-    batch: batch_types[Math.ceil(Math.random() * batch_types.length) - 1],
-  }
-  if(testData.user_role === "contestant"){
-    testData.my_stats = {
-      total_points:Math.floor(Math.random() * (40 - 0 + 1)) + 0,
-      total_attempts:Math.floor(Math.random() * (15 - 0 + 1)) + 0,
-      wallet_balance:Math.floor(Math.random() * (4000 - 500 + 1)) + 500,
-      amount_spent:Math.floor(Math.random() * (6000 - 500 + 1)) + 500,
-      total_votes:Math.floor(Math.random() * (100 - 10 + 1)) + 10,
+  for (let i = 0; i <= num; i++) {
+    const testData = {
+      firstname: faker.name.firstName(),
+      lastname: faker.name.lastName(),
+      username: `${faker.name.firstName()}-${faker.name.lastName()}`,
+      password: "123456",
+      email: faker.internet.email(),
+      user_role:
+        user_role_types[Math.ceil(Math.random() * user_role_types.length) - 1],
+      batch: batch_types[Math.ceil(Math.random() * batch_types.length) - 1]
+    };
+    if (testData.user_role === "contestant") {
+      testData.my_stats = {
+        total_points: Math.floor(Math.random() * (40 - 0 + 1)) + 0,
+        total_attempts: Math.floor(Math.random() * (15 - 0 + 1)) + 0,
+        wallet_balance: Math.floor(Math.random() * (4000 - 500 + 1)) + 500,
+        amount_spent: Math.floor(Math.random() * (6000 - 500 + 1)) + 500,
+        total_votes: Math.floor(Math.random() * (100 - 10 + 1)) + 10
+      };
     }
+    user_seed_data.push(testData);
   }
-  user_seed_data.push(testData)
-}
+  return user_seed_data;
+};
 
-module.exports.user_seed_data
+module.exports = createSeedData;
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 // const axios = require('axios');
-const dotenv = require('dotenv/config');
-const routes = require('./routes/api');
-const {braandlySeeder} = require('./middlewares/seeding');
-const User = require('./models/userModel');
-const {user_seed_data} = require('./data/user_data');
-const {blog_seed_data} = require('./data/blog_data');
-const Audition = require('./models/auditionModel');
-const {questionFetchQuestion} = require('./data/audition_data');
-const Blog = require('./models/blogModel');
-const {blogFetch} = require('./data/blog_data');
-
+const dotenv = require("dotenv/config");
+const routes = require("./routes/api");
+const { braandlySeeder } = require("./middlewares/seeding");
+const User = require("./models/userModel");
+const createSeedData = require("./data/user_data");
+const { blog_seed_data } = require("./data/blog_data");
+const Audition = require("./models/auditionModel");
+const { questionFetchQuestion } = require("./data/audition_data");
+const Blog = require("./models/blogModel");
+const { blogFetch } = require("./data/blog_data");
 
 
 
@@ -26,69 +25,61 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-
 //Routes Handler
-app.get("/", (req, res)=>{
-  res.send("Welcome")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
-app.use('/api', routes)
-
+app.use("/api", routes);
 
 app.use("*", (req, res) => {
-res.status(404).send("Error 404")
-})
-
+  res.status(404).send("Error 404");
+});
 
 // database connection
-// const dbURI = 'mongodb+srv://Fovero:Fovero21biblestar@bible-star-tv.kcjzu.mongodb.net/biblestars?retryWrites=true&w=majority';
-const dbURI = process.env.DB;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+const dbURI =
+  "mongodb+srv://Fovero:Fovero21biblestar@bible-star-tv.kcjzu.mongodb.net/biblestars?retryWrites=true&w=majority";
+// const dbURI = process.env.DB;
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     console.log("Connected to db");
-  
+    let user_seed_data = createSeedData(1000);
+
     // console.log({audition_seed_data})
-    // console.log(user_seed_data)
-    // braandlySeeder(user_seed_data, User)
+    // console.log({user_seed_data})
+    braandlySeeder(user_seed_data, User)
 
-    let audition_seed_data;
+        let audition_seed_data;
 
-(async function() {
-  try {
-    audition_seed_data = await questionFetchQuestion();
-    braandlySeeder(audition_seed_data, Audition)
-    console.log({audition_seed_data})
-  } catch (e) {
-    return console.log(e);
-  }
-})()
-// console.log({audition_seed_data})
+    (async function() {
+      try {
+        audition_seed_data = await questionFetchQuestion();
+        braandlySeeder(audition_seed_data, Audition)
+        console.log({audition_seed_data})
+      } catch (e) {
+        return console.log(e);
+      }
+    })()
+    // console.log({audition_seed_data})
 
-
-
-(async function() {
-  try {
-    let blog_seed_data = await blogFetch();
-    braandlySeeder(blog_seed_data, Blog)
-    // console.log({blog_seed_data})
-  } catch (e) {
-    return console.log(e);
-  }
-})()
+    (async function() {
+      try {
+        let blog_seed_data = await blogFetch();
+        braandlySeeder(blog_seed_data, Blog)
+        // console.log({blog_seed_data})
+      } catch (e) {
+        return console.log(e);
+      }
+    })()
   })
   .catch((err) => console.log(err));
 
-
-
 //Starting server
-app.listen(process.env.port || 3050, ()=>{
-    console.log("Now Listening for requests")
+app.listen(process.env.port || 3050, () => {
+  console.log("Now Listening for requests");
 });
-
-
 
 //Middlewares
 // app.use(express.static('public'));
 // app.use(express.urlencoded({extended: true}));
-
-
